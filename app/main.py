b83203fb030ab3484c2f65d8f3b4773600471d91
Valentina Jaramillo Raquejo <valjaramilloraq@gmail.com>
2026-04-10 15:50:26 -0500

    Code to initialize the database

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 from typing import Union
 
 from fastapi import FastAPI
+
 from app.routers import libros
+from app.database import engine, Base
+from app.models.libro_db import LibroDB
 
 app = FastAPI()
 
 app.include_router(libros.router)
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 async def root():
